da205231ee4e8f0beaf0ceb8bb5b667797a26f78
Floorii18 <flor.intile@hotmail.com>
2023-10-16 17:38:37 -0300

    siempre uno se olvida de cambiarlo en algun lado

--- FinalProject/RegisterFinalProjectApp/views.py
+++ FinalProject/RegisterFinalProjectApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from .forms import *
 from .models import Avatar
